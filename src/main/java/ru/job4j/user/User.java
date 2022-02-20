843bc1b19174d649c79b41d3a6fc9e49635ec908
Rome <tutbora@gmail.com>
2022-02-21 02:58:59 +0300

    2. Без переопределения equals и hashCode [#1005] I have done.

--- src/main/java/ru/job4j/user/User.java
+++ src/main/java/ru/job4j/user/User.java
@@ -1,15 +1,37 @@
 package ru.job4j.user;
 
-import java.util.Calendar;
+import java.util.*;
 
 public class User {
     private final String name;
     private final int children;
     private final Calendar birthday;
 
+    @Override
+    public String toString() {
+        return "Name:" + name
+                + " Children:" + children
+                + " Birthday:" + birthday.get(Calendar.YEAR)
+                + "." + birthday.get(Calendar.MONTH)
+                + "." + birthday.get(Calendar.DAY_OF_MONTH);
+    }
+
     public User(String name, int children, Calendar birthday) {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    public static void main(String[] args) {
+        Calendar calendar = new GregorianCalendar(1999, Calendar.MARCH, 25);
+        User first = new User("Bob", 3, calendar);
+        User second = new User("Bob", 3, calendar);
+        Map<User, Object> userObjectMap = new HashMap<>();
+        userObjectMap.put(first, new Object());
+        userObjectMap.put(second, new Object());
+
+        for (var entry : userObjectMap.entrySet()) {
+            System.out.println(entry);
+        }
     }
 }
