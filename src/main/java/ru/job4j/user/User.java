8dc660e42f09bb1fa661ed388eb707cff652ec67
Rome <tutbora@gmail.com>
2022-02-23 02:45:03 +0300

    5. Переопределить и equals и hashCode [#1002 #268247] I have done. + Calendar + Date

--- src/main/java/ru/job4j/user/User.java
+++ src/main/java/ru/job4j/user/User.java
@@ -1,5 +1,7 @@
 package ru.job4j.user;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.*;
 
 public class User {
@@ -25,13 +27,26 @@ public class User {
     }
 
     @Override
-    public boolean equals(Object anObject) {
-        return this == anObject;
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return children == user.children
+                && Objects.equals(name, user.name)
+                && Objects.equals(birthday, user.birthday);
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(name, children, birthday);
+    }
+
+    static class Calendaric {
+        final int months = 12;
     }
 
     public static void main(String[] args) {
@@ -41,9 +56,56 @@ public class User {
         Map<User, Object> userObjectMap = new HashMap<>();
         userObjectMap.put(first, new Object());
         userObjectMap.put(second, new Object());
+        System.out.println(first.hashCode());
+        System.out.println(second.hashCode());
 
         for (var entry : userObjectMap.entrySet()) {
             System.out.println(entry);
         }
+        System.out.println(".....part 2.....");
+        Date now = new Date();
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(now);
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        int hour = cal.get(Calendar.HOUR);
+        int minute  = cal.get(Calendar.MINUTE);
+        int seconds  = cal.get(Calendar.SECOND);
+        int dayOfWeek   = cal.get(Calendar.DAY_OF_WEEK);
+
+        System.out.println("year is " + year);
+        System.out.println("month is " + month);
+        System.out.println("day is " + day);
+        System.out.println("hour is " + hour);
+        System.out.println("minute is " + minute);
+        System.out.println("second is " + seconds);
+        System.out.println("dayOfWeek is " + dayOfWeek);
+
+        System.out.println(".....part 3.....");
+
+        LocalDate localDate = LocalDate.now();
+        LocalDateTime time = LocalDateTime.now();
+        year = localDate.getYear();
+        month = localDate.getMonth().getValue();
+        day = localDate.getDayOfMonth();
+        hour = time.getHour();
+        minute = time.getMinute();
+        seconds = time.getSecond();
+        dayOfWeek = localDate.getDayOfWeek().getValue();
+
+        System.out.println("year is " + year);
+        System.out.println("month is " + month);
+        System.out.println("day is " + day);
+        System.out.println("hour is " + hour);
+        System.out.println("minute is " + minute);
+        System.out.println("second is " + seconds);
+        System.out.println("dayOfWeek is " + dayOfWeek);
+
+        System.out.println(".....part 4.....");
+
+        Calendaric calendaric = new Calendaric();
+        int months = calendaric.months;
+        System.out.println("months is " + months);
     }
 }
